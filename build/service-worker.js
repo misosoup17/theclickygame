ce3ebf74f9254fdee9114202d47eca76ea3ec85b
misosoup17 <misagaspar@icloud.com>
2019-12-19 18:09:57 -0800

    Update service-worker.js

--- build/service-worker.js
+++ build/service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/misosoup17/theclickygame/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
